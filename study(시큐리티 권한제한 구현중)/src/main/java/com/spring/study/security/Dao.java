ae091120ea45d3682e069e849c02de7725563f66
doridorijamjam2 <hoc1018@naver.com>
2021-12-05 23:49:44 +0900

    개인파트 완료

--- study(시큐리티 권한제한 구현중)/src/main/java/com/spring/study/security/Dao.java
+++
@@ -1,8 +0,0 @@
-package com.spring.study.security;
-
-import java.util.ArrayList;
-
-public interface Dao {
-	public UserDetailsDto selectUser(String username);
-	public ArrayList<String> getAuthList(String username);
-}
